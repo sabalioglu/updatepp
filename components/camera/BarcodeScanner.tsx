fc57753e2396c6f8993eda9bdec9a6e9f0ed73a0
sabalioglu <103506816+sabalioglu@users.noreply.github.com>
2025-06-12 11:31:53 +0300

    Updated BarcodeScanner.tsx

--- components/camera/BarcodeScanner.tsx
+++ components/camera/BarcodeScanner.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Alert, Platform, TextInput } from 'react-native';
 import { theme } from '@/constants/theme';
-import { X, Package, Zap, Search, AlertCircle } from 'lucide-react-native';
+import { X, Package, Zap, Search, CircleAlert as AlertCircle } from 'lucide-react-native';
 
 interface BarcodeScannerProps {
   visible: boolean;

da027da6b8e645d5425aa6a4f2ece5dfe7c42420
sabalioglu <103506816+sabalioglu@users.noreply.github.com>
2025-06-13 12:23:20 +0300

    Deleted babel.config.js

--- utils/idGenerator.ts
+++
@@ -1,7 +0,0 @@
-import { nanoid } from 'nanoid/non-secure';
-
-/**
- * Generate a unique ID using nanoid (non-secure version)
- * This works across both web and native platforms without requiring crypto.getRandomValues()
- */
-export const generateUniqueId = () => nanoid();
